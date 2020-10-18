842e9784fb5ae2cae7d1f33668995369b8c0445f
Glavo <zjx001202@126.com>
2020-10-18 23:26:35 +0800

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+## Kala Ansi 
+
+[![Build Status](https://travis-ci.com/Glavo/kala-base.svg?branch=master)](https://travis-ci.com/Glavo/kala-base)
+[ ![Download](https://api.bintray.com/packages/glavo/maven/kala-base/images/download.svg) ](https://bintray.com/glavo/maven/kala-base/_latestVersion)
+
+
---
+++ src/main/java/asia/kala/EmptyTuple.java
@@ -0,0 +1,74 @@
+package asia.kala;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.IntFunction;
+
+/**
+ * A tuple of no elements.
+ *
+ * @author Glavo
+ */
+public final class EmptyTuple extends Tuple {
+
+    private static final long serialVersionUID = -6688467811848818630L;
+
+    public static final EmptyTuple INSTANCE = new EmptyTuple();
+
+    private EmptyTuple() {
+    }
+
+    //
+    // -- Tuple
+    //
+
+    @Override
+    public final int arity() {
+        return 0;
+    }
+
+    @Override
+    public final <U> U elementAt(int index) {
+        throw new IndexOutOfBoundsException("EmptyTuple.elementAt()");
+    }
+
+    @NotNull
+    @Override
+    public final <U> U[] toArray(@NotNull IntFunction<U[]> generator) {
+        return generator.apply(0);
+    }
+
+    @NotNull
+    @Override
+    public final <H> Tuple1<H> cons(H head) {
+        return new Tuple1<>(head);
+    }
+
+    //
+    // -- Object
+    //
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public final int hashCode() {
+        return Tuple.HASH_MAGIC;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public final String toString() {
+        return "()";
+    }
+
+    //
+    // -- Serializable
+    //
+
+    private Object readResolve() {
+        return INSTANCE;
+    }
+}
